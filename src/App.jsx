d8b547d1715cbac3f08fe1dada3df772faf4c73f
Nikhil Satya Vardhan Kada <nikhilsatyavardhan@gmail.com>
2026-04-05 15:57:16 +0530

    Fix React Router mapping for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <div 
         className="app-background"
         style={{ filter: `blur(${blurValue}px)` }}
